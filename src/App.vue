6c253b65ee67d63fa3e7df9590f7fb04341e55cd
kaczor6418 <k.kaczynski97@gmail.com>
2020-11-15 19:29:22 +0100

    chore(store): add dumb test to check is store working

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,26 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { diceStore } from '@/store/store';
+import { diceStoreConfig } from '@/store/DiceGameStore/DiceGameStoreConfig';
+import { DiceHistoryItem } from '@/store/DiceGameStore/interfaces/DiceHistoryItem';
+import { DiceGameGuess } from '@/common/Enums';
 
 @Component({})
 export default class App extends Vue {
   constructor() {
     super();
   }
+
+  mounted(): void {
+    diceStore.initializeStore(diceStoreConfig, this.setUpGameData);
+  }
+
+  private setUpGameData = (diceHistoryItem: DiceHistoryItem) => {
+    for (let i = 0; i < 31; i++) {
+      diceStore.addNewDiceHistoryItem({ dice: 1, scoreDelta: 0.1, guess: DiceGameGuess.MORE });
+    }
+  };
 }
 </script>
 
