7d5b4632afea4424759c2030a8ad317616d30275
kaczor6418 <k.kaczynski97@gmail.com>
2020-11-15 19:58:08 +0100

    refactor(dumbTests): remove dumb tests

--- src/App.vue
+++ src/App.vue
@@ -9,32 +9,18 @@ import { Component, Vue } from 'vue-property-decorator';
 import { diceStore } from '@/store/store';
 import { diceStoreConfig } from '@/store/DiceGameStore/DiceGameStoreConfig';
 import { DiceHistoryItem } from '@/store/DiceGameStore/interfaces/DiceHistoryItem';
-import { DiceGameGuess } from '@/common/Enums';
 import { EventBus } from '@/EventBus';
 import { Channels } from '@/common/Channels';
 
 @Component({})
 export default class App extends Vue {
-  constructor() {
-    super();
-    this.initializeSubscriptions();
-  }
-
   mounted(): void {
     diceStore.initializeStore(diceStoreConfig, this.setUpGameData);
   }
 
-  private initializeSubscriptions(): void {
-    EventBus.$on(Channels.DICE_GAME_STATE, (payload: any) => console.log('DICE_GAME_STATE', payload));
-    EventBus.$on(Channels.GAME_OVER, () => console.log('GAME_OVER'));
-    EventBus.$on(Channels.DICE_GAME_STORE_READY, () => console.log('DICE_GAME_STORE_READY'));
-  }
-
   private setUpGameData = (diceHistoryItem: DiceHistoryItem) => {
+    EventBus.$emit(Channels.DICE_GAME_STATE, diceHistoryItem);
     EventBus.$emit(Channels.DICE_GAME_STORE_READY);
-    for (let i = 0; i < 31; i++) {
-      diceStore.addNewDiceHistoryItem({ dice: 1, scoreDelta: 0.1, guess: DiceGameGuess.MORE });
-    }
   };
 }
 </script>
