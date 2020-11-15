ecfa6ee97b5b754fd2ad80fc41421a8412e6976d
kaczor6418 <k.kaczynski97@gmail.com>
2020-11-15 21:41:34 +0100

    feat(component): add GameProgress component

--- src/components/GameProgress.vue
+++ src/components/GameProgress.vue
@@ -1,14 +1,63 @@
 <template>
   <div>
-    <h1>Game progress</h1>
+    <strong>{{ round }}</strong>
+    <b>|</b>
+    <strong>{{ score }}</strong>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { EventBus } from '@/EventBus';
+import { Channels } from '@/common/Channels';
+import { DiceHistoryItem } from '@/store/DiceGameStore/interfaces/DiceHistoryItem';
 
-@Component({})
-export default class GameProgress extends Vue {}
+@Component
+export default class GameHistory extends Vue {
+  private currentRound = 0;
+  private currentScore = 0;
+
+  constructor() {
+    super();
+    this.initializeSubscriptions();
+  }
+
+  get round(): string {
+    return `Round: ${this.currentRound}`;
+  }
+
+  get score(): string {
+    return `Score: ${this.currentScore}`;
+  }
+
+  beforeDestroy(): void {
+    EventBus.$off(Channels.DICE_GAME_STATE, this.setGameState);
+  }
+
+  initializeSubscriptions(): void {
+    EventBus.$on(Channels.DICE_GAME_STATE, this.setGameState);
+  }
+
+  private setGameState({ score, round }: DiceHistoryItem): void {
+    this.currentRound = round;
+    this.currentScore = score;
+  }
+}
 </script>
 
-<style lang="scss"></style>
+<style scoped lang="scss">
+div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 1rem;
+  margin-bottom: 1.5rem;
+  font-size: 2rem;
+  background-color: var(--color-primary-dark);
+  box-shadow: var(--shadow-bottom);
+
+  b {
+    color: var(--color-accent-1);
+  }
+}
+</style>
