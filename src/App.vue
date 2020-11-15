1a16a2e06d884297cfdafb34e9d3d211c01be3d9
kaczor6418 <k.kaczynski97@gmail.com>
2020-11-15 18:20:54 +0100

    chore(project): change vue version to vue 2

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="app">
+    <h1>Dice game</h1>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { Component, Vue } from 'vue-property-decorator';
 
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld
+@Component({})
+export default class App extends Vue {
+  constructor() {
+    super();
   }
-});
+}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
 
-createApp(App)
-  .use(store)
-  .mount("#app");
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app');
